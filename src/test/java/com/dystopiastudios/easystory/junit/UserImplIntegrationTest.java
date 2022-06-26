c6b1542251d6ca7719c3571c1060b2c9f590f975
HR-2019 <56949560+HR-2019@users.noreply.github.com>
2022-06-25 20:04:52 -0500

    update: add bean in UserImplIntegrationTest.java

--- src/test/java/com/dystopiastudios/easystory/junit/UserImplIntegrationTest.java
+++ src/test/java/com/dystopiastudios/easystory/junit/UserImplIntegrationTest.java
@@ -13,6 +13,8 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.TestConfiguration;
 import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.context.annotation.Bean;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.api.Assertions.catchThrowable;
@@ -27,15 +29,26 @@ public class UserImplIntegrationTest {
     @Autowired
     private UserService userService;
 
+    @Autowired
+    PasswordEncoder passwordEncoder;
+
     @MockBean
     private SubscriptionRepository subscriptionRepository;
 
     @TestConfiguration
     static class UserImplIntegrationTestConfiguration{
+
         @Bean
         public UserService userService(){
             return new UserServiceImpl();
         }
+
+        @Bean
+        PasswordEncoder passwordEncoder()
+        {
+            return new BCryptPasswordEncoder();
+        }
+
     }
 
     @Test
